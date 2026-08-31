639bb5eef5dc230863e6c451e624e87838a38b33
Alessandra Souza <allemoreirasouza89@gmail.com>
2026-08-31 15:15:10 +0100

    feat: adiciona fundamentos de JavaScript

---
+++ Aula1.js
@@ -0,0 +1,41 @@
+//Cria uma caixa chamada nome e guarda Alessandra dentro dela//
+// const serve para declarar uma variável cujo valor não pretendo reatribuir//
+
+// const nome = 'Alessandra';
+// const email = 'allemoreirasouza89@gmail.com';
+// const idade = 37;
+// const cidade = 'Lisboa';
+// const utilizadorAtivo = true;
+
+// console.log(nome, email, idade, cidade, utilizadorAtivo);
+
+//const → não pretendo atribuir outro valor//
+
+//let  → o valor poderá mudar//
+
+// let tentativasLogin = 0;
+// console.log(tentativasLogin);
+
+// tentativasLogin = 1;
+// console.log(tentativasLogin);
+
+// tentativasLogin = 2;
+// console.log(tentativasLogin);
+
+// =   "guarda isto"
+// ===  "isto é igual àquilo?"
+// if → SE
+// ( )  → qual é a pergunta?
+// ===   → é exatamente igual?
+// { }  → o que faço se a resposta for TRUE?
+
+let tentativasLogin = 10;
+
+    if(tentativasLogin >= 3){
+        console.log('Utilizador bloqueado')
+
+    }else{
+        console.log('Pode tentar novamente')
+    }
+        
+    
