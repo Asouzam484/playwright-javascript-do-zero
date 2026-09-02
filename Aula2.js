bd2a4125a68ee0bf061f1ecf114405478ddcb5eb
Alessandra Souza <allemoreirasouza89@gmail.com>
2026-09-02 11:16:44 +0100

    feat: adiciona funcoes e operadores logicos

---
+++ Aula2.js
@@ -0,0 +1,34 @@
+// const nomeCliente = 'Pedro';
+
+// let idade = 17;
+// let utilizadorAtivo = true;
+
+// if(idade >= 18 && utilizadorAtivo === false){
+
+//     console.log('Acesso permitido');
+
+// }else{
+
+//     console.log('Acesso negado');
+// }
+
+// const temCartao = false;
+// const temConvite = true;
+// //Uma pessoa pode entrar numa festa se tiver cartão de acesso OU convite.//
+
+//     if(temCartao === true || temConvite === true){
+
+//         console.log('Entrada permitida')
+
+//     }else{
+
+//         console.log('Entrada negada')
+//     }
+
+function calcularTotal(preco, quantidade) {
+  return preco * quantidade;
+}
+
+const total = calcularTotal(10, 3);
+
+console.log(total);
